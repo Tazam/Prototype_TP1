a78192545a7c2bbdb608944ce6f3a7c710f539c4
uapv1500078 <uapv1500078@ge-st5-111.univ-avignon.fr>
2018-02-14 17:16:24 +0100

    test

--- src/tp1/FXMLDocumentController.java
+++ src/tp1/FXMLDocumentController.java
@@ -12,6 +12,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ToolBar;
+import javafx.scene.control.Tooltip;
+
+
+
 
 /**
  *
@@ -29,6 +37,36 @@ public class FXMLDocumentController implements Initializable {
     private Button buttonSlideshow;
     @FXML
     private Button buttonDirectory;
+    @FXML
+    private Button buttonNewKeyword;
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private Menu menuFile;
+    @FXML
+    private MenuItem menuItemClose;
+    @FXML
+    private Menu menuEdit;
+    @FXML
+    private MenuItem menuItemDelete;
+    @FXML
+    private Menu menuHelp;
+    @FXML
+    private MenuItem menuItemAbout;
+    @FXML
+    private Menu menuLanguage;
+    @FXML
+    private MenuItem menuItemEnglish;
+    @FXML
+    private MenuItem menuItemFrench;
+    @FXML
+    private MenuItem menuItemRussian;
+    @FXML
+    private ToolBar toolBar;
+    @FXML
+    private Tooltip tooltipLaunchSlideshow;
+    
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
