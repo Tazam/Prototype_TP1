a8c8a6cc56b17812ee7f7fd5a2c1a618ac513f21
uapv1500078 <uapv1500078@ge-st5-111.univ-avignon.fr>
2018-02-20 10:33:58 +0100

    writer

--- src/tp1/PictureManager.java
+++ src/tp1/PictureManager.java
@@ -4,15 +4,22 @@
  * and open the template in the editor.
  */
 package tp1;
+import java.io.BufferedWriter;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Tazam
  */
 public class PictureManager {
+    // map contient des images avec les mots clés assosiés
     protected Map <Picture,List<String>> collection;
     public List<Picture> currentPictures;
     
@@ -37,10 +44,30 @@ public class PictureManager {
     
     public void save()
     {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("colSaveKW.txt", true))) {
+            
+            // pour chaque element de map
+            collection.forEach((img ,setKeyWords)->{
+                try {
+                    bw.write(img.getName());
+                    bw.newLine();
+                    bw.write(img.getPath());
+                    bw.newLine();
+                    
+                    // pour chaque mot clé
+                    for(String keyWord : setKeyWords){
+                        bw.write(keyWord);
+                        bw.newLine();
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(PictureManager.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
         
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    
-    
-    
+       
 }
